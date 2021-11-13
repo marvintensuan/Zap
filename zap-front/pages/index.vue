e12f32050656afe8d607f7b62fcaa3a4d403af8c
Marvin <mdtensuan_dev@protonmail.com>
2021-11-14 00:08:29 +0800

    Initial commit.

---
+++ zap-front/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to the Zap Inventory Software!
+        </v-card-title>
+        <v-card-text>
+          <p>Zap is an Inventory Web App where you can pull up reports from a database and modify data using Excel Sheets!</p>
+          <p>This is definitely not a rip-off of a famous ERP software provider.</p>
+          <p>This website is powered by Nuxt JS + Vuetify on the frontend and some backend framework.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; Marvin</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://github.com/marvintensuan/Zap"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Zap on GitHub
+          </a>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
